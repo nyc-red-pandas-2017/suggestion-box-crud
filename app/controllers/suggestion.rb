b6b78d0b8714415865ad129f86c1833c9437d1c5
Amber Maillard <amber.maillard23@gmail.com>
2017-10-11 18:38:36 -0400

    Attempt to get update feature to work

--- app/controllers/suggestion.rb
+++ app/controllers/suggestion.rb
@@ -19,3 +19,22 @@ get '/suggestions/:id' do
   @suggestion = Suggestion.find_by(id: params[:id])
   erb :"suggestions/show"
 end
+
+delete '/suggestions/:id' do
+    @suggestion = Suggestion.find_by(id: params[:id])
+    if @suggestion.user_id == current_user.id
+      @suggestion.destroy!
+      redirect "/users/#{current_user.id}"
+  end
+end
+
+get '/suggestions/:id/edit' do
+  @suggestion = Suggestion.find_by(id: params[:id])
+  erb :"suggestions/edit"
+end
+
+put '/suggestions/:id' do
+  @suggestion = Suggestion.find_by(id: params[:id])
+  @suggestion.update
+  redirect "/users/#{current_user.id}"
+end

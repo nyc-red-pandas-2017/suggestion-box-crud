883a2917016a3b5377b0a5e7a42cb7a91d245f19
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-10-11 14:59:03 -0400

    User Login in working, sign up still has bug

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -9,3 +9,18 @@ get "/users/:id" do
   redirect "/" unless authorized?(@user)
   erb :"users/show"
 end
+
+get "/user/new" do
+  erb :'users/new'
+end
+
+post '/users' do
+    @user = User.new(params[new_user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'users/new'
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Suggestion.delete_all
 User.delete_all
 
-thomas = User.create!(username: "thomas", hash_password: "123456")
-penn = User.create!(username: "penn", hash_password: "123456")
-david = User.create!(username: "david", hash_password: "123456")
+thomas = User.create!(username: "thomas", password: "123456")
+penn = User.create!(username: "penn", password: "123456")
+david = User.create!(username: "david", password: "123456")
 
 S1 = Suggestion.create!(title: "Get Sleep", body: "It is not a government conspiracy!", user: thomas)
 S2 = Suggestion.create!(title: "Eat Dumplings", body: "The are afforable and delicous!", user: thomas)

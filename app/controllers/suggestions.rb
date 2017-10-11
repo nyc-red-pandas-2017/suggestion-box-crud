7c76c6ed4a22ed5285bb2f33a586d9507bb8c2c5
Andres Arias <email": null>
2017-10-11 14:24:25 -0400

    Fully working suggestion CRUD app

--- app/controllers/suggestions.rb
+++ app/controllers/suggestions.rb
@@ -24,5 +24,26 @@ get '/suggestions/:id' do
 end
 
 get '/suggestions/:id/edit' do
-  "edit form"
+  @suggestion = Suggestion.find_by(id: params[:id])
+  erb :'suggestions/edit'
+end
+
+put '/suggestions/:id' do
+  @suggestion = Suggestion.find_by(id: params[:id])
+  @suggestion.assign_attributes(params[:suggestion])
+  if @suggestion.save
+    redirect "/"
+  else
+    @errors = @suggestion.errors.full_messages
+    erb :'suggestion/edit'
+  end
+end
+
+delete "/suggestions/:id" do
+  puts params
+  puts "**************************************"
+  p @suggestion = Suggestion.find_by(id: params[:id])
+  puts "**************************************"
+  @suggestion.destroy!
+  redirect "/"
 end

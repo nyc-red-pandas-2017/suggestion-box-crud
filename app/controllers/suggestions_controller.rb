698722d6756b855ed06a3fbd487b0268c0debb8c
David Skaggs <davidtskaggs@gmail.com>
2017-10-12 16:21:41 -0400

    push for review

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -84,5 +84,10 @@ delete '/suggestions/:id' do
   else
     redirect '/suggestions'
   end
+end
 
+post '/suggestions/:id/vote' do
+  suggestions = Suggestion.find(params[:id])
+  suggestions.vote.create(value: 1)
+  redirect "/suggestions"
 end
